f9c03c984de2d596740de83f315f66c41bba2727
MartaAnastazja <mcm.czarnecka@gmail.com>
2019-08-16 09:21:21 +0200

    Remove :

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './WhosOnlineList.css'
 
 class RoomList extends React.Component {
     render () {
         const orderedRooms = [...this.props.rooms].sort((a, b) => a.id > b.id)
         return (
             <div className="rooms-list">
+                <h3 className="room-title">Your rooms</h3>
                 <ul>
-                <h3>Your rooms:</h3>
-                    {orderedRooms.map(room => {
-                        const active = room.id === this.props.roomId ? 'active' : '';
-                        return (
-                            <li key={room.id} className={"room " + active}>
-                                <a
-                                    onClick={() => this.props.subscribeToRoom(room.id)}
-                                    href="#">
-                                    # {room.name}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
+                        {orderedRooms.map(room => {
+                            const active = room.id === this.props.roomId ? 'active' : '';
+                            return (
+                                <li key={room.id} className={"room " + active}>
+                                    <a
+                                        onClick={() => this.props.subscribeToRoom(room.id)}
+                                        href="#">
+                                        # {room.name}
+                                    </a>
+                                </li>
+                            )
+                        })}
+                    </ul>
             </div>
         )
     }
