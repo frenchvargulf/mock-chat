e7abbddba2b42b198da7ed6c02ce5c91149d701c
frenchvargulf <marta.s.czarnecka@gmail.com>
2019-08-20 18:25:02 +0200

    Best practices update and main styles update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
       currentUsername: '',
       visibleScreen: "getUsernameScreen"
     }
-    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
   }
   onUsernameSubmitted(username) {
     fetch('/users', {
@@ -30,7 +29,7 @@ class App extends Component {
   }
   render() {
       if (this.state.visibleScreen === 'getUsernameScreen') {
-        return <GetUsername onSubmit={this.onUsernameSubmitted} />
+        return <GetUsername onSubmit={(e) => this.onUsernameSubmitted(e)} />
       }
       if (this.state.visibleScreen === 'Chat') {
         return <Chat currentUsername={this.state.currentUsername} />
