cb38a225c02a55496fa4bd7e2825f20339d8296f
frenchvargulf <marta.s.czarnecka@gmail.com>
2019-08-20 18:28:27 +0200

    Prepare button

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import './MenuButton.css';
+
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
  
 class MenuButton extends Component {
   render() {
