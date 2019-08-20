8f504ef33b24baaee59e1281ea25c76d92648eb3
frenchvargulf <marta.s.czarnecka@gmail.com>
2019-08-20 18:27:58 +0200

    Add painitng space

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,31 @@ import "./Menu.css";
 import WhosOnlineList from './WhosOnlineList';
 import RoomList from './RoomList';
 import NewRoomForm from './NewRoom';
- 
+import Canvas from './components';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class Paint extends Component {
+
+  
+  render(){
+    return (
+      <div>
+        <h3 style={{ textAlign: 'center' }}>Painting Space</h3>
+        <div className="main">
+          <div className="color-guide">
+          <button onClick={this.props.handleClick}>Switch</button>
+            <h5>Color Guide</h5>
+            <div className="user user"></div>
+            <div className="user guest"></div>
+          </div>
+          
+        </div>
+      </div>
+    )
+  }
+}
+
+
 class Menu extends Component {
   render() {
     var visibility = "hide";
@@ -16,16 +40,29 @@ class Menu extends Component {
       <div id="flyoutMenu" 
         onMouseDown={this.props.handleMouseDown} 
         className={visibility} >
-            <WhosOnlineList
-                currentUser={this.props.currentUser}
-                    users={this.props.currentRoom.users}
-                    className="whosOnlineListContainer"
-            />
-            <RoomList
-                        subscribeToRoom={this.props.subscribeToRoom}
+            <div className="fly-container">
+                <WhosOnlineList
+                    currentUser={this.props.currentUser}
+                        users={this.props.currentRoom.users}
+                        className="whosOnlineListContainer"
+                  sendDM={this.props.sendDM}
+                />
+                <RoomList
+                            currentUser={this.props.currentUser}
+                            subscribeToRoom={this.props.subscribeToRoom}
                             rooms={this.props.rooms}
-                        roomId={this.props.roomId} />
-            <NewRoomForm createRoom={this.props.createRoom} roomId={this.props.roomId} currentUser={this.props.currentUser} />
+                        
+                            />
+                <NewRoomForm createRoom={this.props.createRoom} currentUser={this.props.currentUser} />
+                <Paint handleClick={this.props.handleClick} />
+                </div>
+            {/* <Router>
+            
+
+
+            <Link to="/paint/">Paint</Link>
+            <Route path="/paint/" component={Paint} />
+            </Router> */}
       </div>
     );
   }
