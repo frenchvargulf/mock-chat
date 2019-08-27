1bee80ce5c952823d1efe51220fbb5eadc755c87
frenchvargulf <marta.s.czarnecka@gmail.com>
2019-08-27 21:57:17 +0200

    Change message styles for better display

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
 import './Message.css'
 
+const Img = ({src}) => {
+    return (<img src={src} alt={src}></img>)
+}
+
 class Message extends Component{
     render () {
-        return ( 
-            <div >
-                <li key={this.props.id} className="message-cnt" >
-                    <p className="message-sender"> 
-                        { this.props.senderId }
-                    </p>
-                    <p className="Message" >
+        return (
+            <li key={this.props.id} className="message-cnt" >
+                <div className="avatar-container">
+                    <img style={{width:"50px", height:"50px", backgroundImage:"grey"}}></img>
+                </div>
+                <div className="info-container">
+                    <div className="sender">
+                    <span className="message-time">{this.props.time}</span> 
+                        <div className="message-sender"> 
+                            { this.props.senderId }
+                        </div>
+                    </div>
+                    <div className="Message" >
                         { this.props.text } 
-                    </p>
-                </li>
-            </div>
+                        { this.props.media != null? <Img src={this.props.media.link}/>:null}
+                    </div>
+                </div>
+            </li>
         );
     }
 };
